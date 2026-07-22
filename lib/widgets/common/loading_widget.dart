41eea89b6d86831a1cef678491be0ace6d4dcf4a
martinezn3gr0 <martinezn3gr0o@gmail.com>
2026-07-22 04:01:58 -0600

    Add loading widget

---
+++ lib/widgets/common/loading_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class LoadingWidget extends StatelessWidget {
+  final String? message;
+  final bool useShimmer;
+  
+  const LoadingWidget({
+    Key? key,
+    this.message,
+    this.useShimmer = false,
+  }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    if (useShimmer) {
+      return Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: ListView(
+          children: List.generate(
+            5,
+            (index) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+    
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          if (message != null) ...[
+            const SizedBox(height: 16),
+            Text(
+              message!,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
